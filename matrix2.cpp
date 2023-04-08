16d55b545b9d7e01253da1224c44d0bf084dcbf4
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-04-08 20:05:10 +0600

    Jana

--- matrix2.cpp
+++ matrix2.cpp
@@ -3,12 +3,23 @@ using namespace std;
 
 int main(){
 
-   int A [3][3] = {5, 7, 0, 4, 3, 1, 9, 1, 1}; 
+   int A [3][3] = {5, 7, 0, 4, 3, 1, 9, 1, 4}; 
    int B [3][1] = {7, 2, 4};
    int C [3][3]; 
    int D [3][1]; 
-
-    
+   float A_inv[3][3];
+   
+   // Finding the inverse of matrix A
+   float determinant = 0;
+   for (int i = 0; i < 3; i++) {
+       determinant = determinant + (A[0][i] * (A[1][(i+1)%3] * A[2][(i+2)%3] - A[1][(i+2)%3] * A[2][(i+1)%3]));
+   }
+   for (int i = 0; i < 3; i++) {
+       for (int j = 0; j < 3; j++) {
+           A_inv[i][j] = ((A[(j+1)%3][(i+1)%3] * A[(j+2)%3][(i+2)%3]) - (A[(j+1)%3][(i+2)%3] * A[(j+2)%3][(i+1)%3])) / determinant;
+       }
+   }
+   
    //Matricalardyn bastapqy turi
     cout << "\nBastapqy turi: " << endl;
     cout << "\nA: " << endl << endl;
